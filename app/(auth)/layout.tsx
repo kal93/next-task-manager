07fba6b0ac987a53ce2094948d1b351bf32a77c7
maaz <maaz2dt@gmail.com>
2023-05-20 11:57:39 +0530

    layouts and components

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import GlassPane from "@tm/components/GlassPane";
+import '@tm/styles/global.css';
+
+export default function AuthRootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head></head>
+
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">{children}</GlassPane>
+      </body>
+    </html>
+  );
+}
---
+++ components/GlassPane.tsx
@@ -0,0 +1,17 @@
+import clsx from "clsx";
+import { FC } from "react";
+
+const GlassPane: FC = ({ children, className }) => {
+  return (
+    <div
+      className={clsx(
+        "glass rounded-2xl border-solid border-2 border-gray-200",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default GlassPane;
