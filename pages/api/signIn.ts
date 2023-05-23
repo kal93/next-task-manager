996af71e96adf1bf0dde621f530390e277dfc02f
maaz <maaz2dt@gmail.com>
2023-05-23 11:47:06 +0530

    register and sign in apis and util fns

---
+++ pages/api/signIn.ts
@@ -0,0 +1,37 @@
+import { comparePassword, createJWT } from "@tm/lib/auth";
+import { db } from "@tm/lib/db";
+import { serialize } from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function SignIn(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const user = await db.user.findUnique({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    const isUser = comparePassword(req.body.password, user?.password);
+    if (isUser) {
+      const jwt = await createJWT(user);
+
+      res.setHeader(
+        "Set-Cookie",
+        serialize(process.env.COOKIE_NAME, jwt, {
+          httpOnly: true,
+          path: "/",
+          maxAge: 60 * 60 * 24 * 7,
+        })
+      );
+
+      res.status(201);
+      res.end();
+    } else {
+      res.status(402);
+      res.end();
+    }
+  }
+}
